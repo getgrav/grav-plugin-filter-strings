39835db1fc9fc8529191c8ea2be9d883400e5dca
= <cosgrove.tyler.98@gmail.com>
2015-10-15 21:29:56 -0400

    Initial Commit

---
+++ filterStrings.php
@@ -0,0 +1,44 @@
+<?php
+namespace Grav\Plugin;
+use Grav\Common\Data;
+use Grav\Common\Plugin;
+use Grav\Common\Grav;
+use Grav\Common\Page\Page;
+use RocketTheme\Toolbox\Event\Event;
+class FilterStringsPlugin extends Plugin
+{
+    /**
+     * @var FilterStringsPlugin
+     */
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
+        ];
+    }
+    /**
+     * Initialize configuration
+     */
+    public function onPluginsInitialized()
+    {
+        if ($this->isAdmin() && !$this->config->get('plugins.filterstrings.enabled_in_admin', false) && !$this->config->get('plugins.filterstrings.enabled', false)) {
+            $this->active = false;
+            return;
+        }
+        require_once(__DIR__.'/vendor/autoload.php');
+        $this->enable([
+            'onTwigExtensions' => ['onTwigExtensions', 0]
+        ]);
+    }
+    /**
+     * Add Twig Extensions
+     */
+    public function onTwigExtensions()
+    {
+        require_once(__DIR__.'/twig/FilterStringsTwigExtension.php');
+        $this->grav['twig']->twig->addExtension(new FilterStringsTwigExtension());
+    }
+}
---
+++ twig/FilterStringsTwigExtension.php
@@ -0,0 +1,34 @@
+<?php
+namespace Grav\Plugin;
+use \Grav\Common\Grav;
+use Stringy\Stringy as S;
+use function Stringy\create as s;
+
+class FilterStringsTwigExtension extends \Twig_Extension
+{
+    protected $config;
+    public function __construct()
+    {
+        $this->config = Grav::instance()['config'];
+    }
+    /**
+     * Returns extension name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'FilterStringsTwigExtension';
+    }
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('*_string', [$this, 'stringFilter']),
+        ];
+    }
+    public function stringFilter($action, $data)
+    {
+        $a = call_user_func_array('Stingy\Stringy::'.$action, $data);
+        return $action;
+    }
+}
